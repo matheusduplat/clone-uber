124b360856c4e70a0a8a804d98d0c096271c0db9
matheus.duplat <matheus.duplat@soterotech.com.br>
2025-12-16 17:26:15 -0300

    3:00:00

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { Platform, Text } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 export function Map() {
+  // const region = {
+
+  // }
   return (
-    <View>
-      <Text>Map Component</Text>
-    </View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType={Platform.OS === "ios" ? "mutedStandard" : undefined}
+      showsPointsOfInterest={false}
+      // initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+      <Text>Map</Text>
+    </MapView>
   );
 }
